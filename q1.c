e36ea62c14ba45ae248e594b93a9460d893e13ff
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2020-11-17 15:52:06 +0530

    handling escape sequences

--- q1.c
+++ q1.c
@@ -41,11 +41,75 @@ int main(int argc, char **argv)
         close(fd[1]);
         read(fd[0], output, sizeof(output));
         char upper_output[MAX_SIZE];
-        int i = 0;
+        int i = 0, j = 0;
         while(output[i])
         {
             int c = output[i];
-            upper_output[i++] = toupper(c);
+            if(c == '\\')
+            {
+                char esc = output[i+1];
+                i+=2;
+                if(esc == 't')
+                {
+                    upper_output[j] = '\t';
+                }
+                else if(esc == 'v')
+                {
+                    upper_output[j] = '\v';
+                }
+                else if(esc == 'a')
+                {
+                    upper_output[j] = '\a';
+                }
+                else if(esc == 'b')
+                {
+                    upper_output[j] = '\b';
+                }
+                else if(esc == 'e')
+                {
+                    upper_output[j] = '\e';
+                }
+                else if(esc == 'f')
+                {
+                    upper_output[j] = '\f';
+                }
+                else if(esc == 'n')
+                {
+                    upper_output[j] = '\n';
+                }
+                else if(esc == 'r')
+                {
+                    upper_output[j] = '\r';
+                }
+                else if(esc == '\\')
+                {
+                    upper_output[j] = '\\';
+                }
+                else if(esc == '\'')
+                {
+                    upper_output[j] = '\'';
+                }
+                else if(esc == '\"')
+                {
+                    upper_output[j] = '\"';
+                }
+                else if(esc == '\?')
+                {
+                    upper_output[j] = '\?';
+                }
+                else
+                {
+                    i--;
+                    upper_output[j] = '\\';
+                }
+                j++;
+            }
+            else 
+            {
+                upper_output[j++] = toupper(c);
+                i++;
+            }
+            // printf("%c ", upper_output[j-1]);
         }
         printf("%s", upper_output);
         close(fd[0]);
