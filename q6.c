a271e47bb8910dd7eb9961c8cf4361d93a358547
Ananya Lohani <ananya19018@iiitd.ac.in>
2020-11-18 19:45:11 +0000

    intermediate

--- q6.c
+++ q6.c
@@ -5,52 +5,53 @@
 #include <unistd.h>
 
 #define N 5
-#define THINK -1
-#define EAT 0
-#define HUNGRY 1
-#define LEFT (pnum + 4) % N
+#define TIME 1
+#define LEFT pnum
 #define RIGHT (pnum + 1) % N
 
 struct philos 
 {
-    int state;
     int num;
     int eaten;
+    pthread_t phil_tid;
 };
 
 sem_t mutex;
-sem_t chop_lock[N];
+sem_t chop[N];
 struct philos phil[N];
 
-void test(int pnum);
-void pick_up_chop(int pnum);
-void put_down_chop(int pnum);
+void pick_left_chop(int pnum);
+void pick_right_chop(int pnum);
 void *phil_thread(void *num);
 int have_all_eaten();
 
 int main(int argc, char **argv)
 {
     sem_init(&mutex, 0, 1);
-    pthread_t phil_tid[N];
 
     for(int i = 0; i < N; i++)
     {
-        sem_init(&chop_lock[i], 0, 0);
-        phil[i].state = THINK;
+        sem_init(&chop[i], 0, 0);
         phil[i].num = i; 
         phil[i].eaten = 0;
     }
     for(int i = 0; i < N; i++)
     {
-        pthread_create(&phil_tid[i], NULL, phil_thread, &phil[i].num);
+        pthread_create(&phil[i].phil_tid, NULL, phil_thread, &phil[i].num);
         // printf("P%d is thinking\n", i + 1);
     }
 
     for(int i = 0; i < N; i++)
     {
-        pthread_join(phil_tid[i], NULL);
+        pthread_join(phil[i].phil_tid, NULL);
     }
     // printf("All philosophers have eaten.\n");
+    sem_destroy(&mutex);
+    for(int i = 0; i < N; i++)
+    {
+        sem_destroy(&chop[i]);
+    }
+    return 0;
 }
 
 void *phil_thread(void *num)
@@ -59,48 +60,30 @@ void *phil_thread(void *num)
     {
         if(have_all_eaten()) break;
         int *i = (int *)num;
-        sleep(3);
-        pick_up_chop(*i);
-        sleep(1);
-        put_down_chop(*i);
+        sleep(TIME);    // thinking
+        pick_left_chop(*i);
+        pick_right_chop(*i);
     }
 }
 
-void test(int pnum)
-{
-    if(phil[pnum].state == HUNGRY 
-    && phil[LEFT].state != EAT 
-    && phil[RIGHT].state != EAT)
-    {
-        phil[pnum].state = EAT;
-        sleep(1);
-        printf("P%d receives F%d, F%d\n", pnum + 1, LEFT + 1, pnum + 1);
-        // printf("P%d is eating\n", pnum + 1);
-        sem_post(&chop_lock[pnum]);
-    }
-}
-
-void pick_up_chop(int pnum)
+void pick_left_chop(int pnum)
 {
     sem_wait(&mutex);
-    phil[pnum].state = HUNGRY;
-    // printf("P%d is hungry\n", pnum + 1);
-    test(pnum);
+    sem_wait(&chop[LEFT]);
+    printf("P%d receives F%d\n", phil[pnum].num + 1, LEFT);
+}
+
+void pick_right_chop(int pnum)
+{
+    sem_wait(&chop[RIGHT]);
     sem_post(&mutex);
-    sem_wait(&chop_lock[pnum]);
-    sleep(1);
-}
 
-void put_down_chop(int pnum)
-{
-    sem_wait(&mutex);
-    phil[pnum].state = THINK;
-    // printf("P%d puts down F%d, F%d\n", pnum + 1, LEFT + 1, pnum + 1);
+    printf("P%d receives F%d, F%d\n", phil[pnum].num + 1, LEFT, RIGHT);
     phil[pnum].eaten++;
-    // printf("P%d is thinking\n", pnum + 1);
-    test(LEFT);
-    test(RIGHT);
-    sem_post(&mutex);
+    sleep(TIME);    //eating
+
+    sem_post(&chop[RIGHT]);
+    sem_post(&chop[LEFT]);
 }
 
 int have_all_eaten()
